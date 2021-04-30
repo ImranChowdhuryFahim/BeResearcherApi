b13b896199007a93f2f50f107656716c574fec10
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-04-30 17:52:28 +0600

    country field added

--- models/StudentCompletedCourse.js
+++ models/StudentCompletedCourse.js
@@ -3,41 +3,45 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const Student = new Schema({
-	id: {
-		type: mongoose.Types.ObjectId,
-		required: true,
-	},
-	firstName: {
-		type: String,
-		required: true,
-	},
-	lastName: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-	institution: {
-		type: String,
-		required: true,
-	},
-	dept: {
-		type: String,
-		required: true,
-	}
+  id: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  institution: {
+    type: String,
+    required: true,
+  },
+  dept: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
 });
 
 const StudentCompletedCourse = new Schema({
-	courseId: mongoose.Types.ObjectId,
-	students: {
-		type: [Student],
-		required: true,
-	},
+  courseId: mongoose.Types.ObjectId,
+  students: {
+    type: [Student],
+    required: true,
+  },
 });
 
 module.exports = mongoose.model(
-	'studentCompleteCourse',
-	StudentCompletedCourse
+  'studentCompleteCourse',
+  StudentCompletedCourse
 );
